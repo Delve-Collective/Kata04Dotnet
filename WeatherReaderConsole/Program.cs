973aa9d48b850434d6377af1c00390adf12d0be2
Cory Koehler <Cory@lithe.technology>
2025-07-17 15:29:28 -0400

    Implement console program to utilize weather service

--- WeatherReaderConsole/Program.cs
+++ WeatherReaderConsole/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace WeatherReaderConsole
+﻿namespace WeatherReaderConsole;
+
+internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        var service = new WeatherService();
+
+        var path = Path.Combine("..", "..", "..", "..", "Kata04Data", "weather.parquet");
+
+        try
         {
-            Console.WriteLine("Hello, World!");
+            var result = await service.RetrieveDayWithSmallestTempSpreadAsync(path);
+            Console.WriteLine($"Day with smallest temperature spread: {result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
 }

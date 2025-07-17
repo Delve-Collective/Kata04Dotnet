2fc187254c53bef63308759c842d17a63cd4d9c1
Cory Koehler <Cory@lithe.technology>
2025-07-17 15:05:11 -0400

    RED intentional - wire up weather service with unit test. test fail as an example of red green refactor

---
+++ UnitTests/WeatherServiceUnitTests.cs
@@ -0,0 +1,18 @@
+﻿namespace UnitTests;
+
+public class WeatherServiceUnitTests
+{
+    [Fact]
+    public async Task ShouldRetrieveDayWithSmallestTempSpread()
+    {
+        // Arrange
+        var service = new WeatherService();
+        var path = Path.Combine("..", "..", "..", "..", "Kata04Data", "weather.parquet");
+
+        // Act
+        var result = await service.RetrieveDayWithSmallestTempSpreadAsync(path);
+
+        // Assert
+        result.Should().Be(14);
+    }
+}
---
+++ WeatherReaderConsole/WeatherService.cs
@@ -0,0 +1,19 @@
+﻿namespace WeatherReaderConsole;
+
+public interface IWeatherService
+{
+    Task<int> RetrieveDayWithSmallestTempSpreadAsync(string parquetPath);
+}
+
+public class WeatherService : IWeatherService
+{
+    public WeatherService()
+    {
+        
+    }
+
+    public async Task<int> RetrieveDayWithSmallestTempSpreadAsync(string parquetPath)
+    {
+        return 10;
+    }
+}

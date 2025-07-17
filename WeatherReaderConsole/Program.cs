5aafd1c6c95aa91f54bde736a65acee958a64562
Cory Koehler <Cory@lithe.technology>
2025-07-17 14:52:59 -0400

    Initalize test project and weather reader console app

---
+++ WeatherReaderConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherReaderConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
